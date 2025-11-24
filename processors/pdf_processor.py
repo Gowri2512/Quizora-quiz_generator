250b624a75ec423669246bc3e3c84253db376eaa
Gowri2512 <gowri25777@gmail.com>
2025-11-24 23:10:02 +0530

    Add PDF processing functionality with pdfplumber

---
+++ processors/pdf_processor.py
@@ -0,0 +1,8 @@
+import pdfplumber
+
+def process_pdf(file):
+    text = ""
+    with pdfplumber.open(file) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text() + "\n"
+    return text
